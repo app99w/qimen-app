6247a57983e41797a77e7af0c9d5d2761c742d27
app99w <1363789262@qq.com>
2026-09-19 01:56:49 +0800

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,8 @@
+进口 { CapacitorConfig } 从……起'@capacitor/cli';
+Const配置: CapacitorConfig={
+  AppID: 'com.qimen.app',
+  AppName: '奇门',
+  webDir: 'Dist',
+  服务器: { androidScheme: 'https' }
+};
+出口 默认 配置;
